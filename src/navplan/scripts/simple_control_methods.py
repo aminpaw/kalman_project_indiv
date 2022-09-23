dd87e3fa310263c78ad8f111a4b75b953fc10750
aminpaw <rocketlee2004@gmail.com>
2022-09-23 16:11:19 +0200

    modified:   src/navplan/scripts/simple_control_methods.py

--- src/navplan/scripts/simple_control_methods.py
+++ src/navplan/scripts/simple_control_methods.py
@@ -5,6 +5,7 @@ import rospy
 
 from std_msgs.msg import Float64MultiArray, Float64
 from modules.sprites import *
+from matplotlib.pyplot import plot
 
 rospy.init_node('pure_pursuit_control')
 long_drive_pub = rospy.Publisher('controls/throttle', Float64, queue_size=10)
@@ -36,6 +37,8 @@ rospy.Subscriber("waypoints", Float64MultiArray, update_waypoint)
 class Controller:
     def __init__(self, L=4.9):
         self.L = L
+        self.error_prev = 0
+        self.error_I = 0
        
     def get_longitudinal_control(self,v_current,v_desired,dt):
         '''
@@ -54,10 +57,13 @@ class Controller:
         throttle_output: float
             Value in the range [-1,1]
         '''
-        gains = [0.1,0.01,0.01]                     #Kp, Ki, Kd
+        gains = [0.19,0.0035,0.4]                     #Kp, Ki, Kd (ideal gains = [0.19,0.0035,0.4])
         error = v_desired - v_current
-
-        throttle_output = sorted((-1, error*gains[0], 1))[1]
+        error_d = (error - self.error_prev)
+        self.error_I += error
+        output = error*gains[0] + self.error_I*gains[1] + error_d*gains[2]
+        throttle_output = sorted((-1, output, 1))[1]
+        self.error_prev = error
         return throttle_output
         pass
     
@@ -79,6 +85,17 @@ class Controller:
         steer_angle: float
             Steering angle in rad
         '''
+
+        current_xy = (current_xy[0] - 0.5*self.L*np.cos(current_yaw)), (current_xy[1] - 0.5*self.L*np.sin(current_yaw))
+        L_d = math.dist(current_xy,next_waypoint)
+        direction = next_waypoint - current_xy
+        unit_dir = direction / np.linalg.norm(direction)
+        heading = np.arccos(np.clip(np.dot(unit_dir, [0,0]), -1.0, 1.0))
+        alpha = heading - current_yaw
+
+        steer_angle = np.arctan((2*self.L*np.sin(alpha))/L_d)
+        return steer_angle
+
         pass
 
     def get_lateral_stanley(self,current_xy,current_yaw,current_speed,next_waypoint):
@@ -105,7 +122,12 @@ class Controller:
        
 controller = Controller()
  
+error_prev = 0
+error_integ = 0
+
+
 rate = 10
+time = 0
 r = rospy.Rate(rate)
 
 while not rospy.is_shutdown():
@@ -117,8 +139,8 @@ while not rospy.is_shutdown():
     
     
     # Longitudinal and lateral control
-    longitudinal_cont = controller.get_longitudinal_control(current_state[3],10,0.1)
-    lateral_cont = 0.5
+    longitudinal_cont = controller.get_longitudinal_control(current_state[3],1,0.1)
+    lateral_cont = controller.get_lateral_pure_pursuit(current_state[[0,1]],current_state[2],curr_waypoint[[0,1]])
 
     # Create longitudinal and lateral messages (of type Float64 imported above)
     throttle = Float64(longitudinal_cont)
@@ -129,5 +151,3 @@ while not rospy.is_shutdown():
     steering_drive_pub.publish(steer)
     print("Torque: {:.2f}, Steering angle: {:.2f}".format(longitudinal_cont,lateral_cont))
     print(current_state)
-    print()
-    
